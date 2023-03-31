bb07ec9ab3006ed139bf2b8dfc12889550914b2d
IttesafunAbian <48613857+IttesafunAbian@users.noreply.github.com>
2023-03-31 09:25:19 +0600

    Add files via upload

---
+++ QuestionsetUI.java
@@ -0,0 +1,31 @@
+package com.quizgame.quizgame;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class QuestionsetUI {
+    @FXML
+
+    public TextField categorytype;
+    @FXML
+    public TextField questionstatement;
+    @FXML
+    public TextField option1;
+    @FXML
+    public TextField option2;
+    @FXML
+    public TextField option3;
+    @FXML
+    public TextField option4;
+    @FXML
+
+    public TextField correctans;
+
+    @FXML
+
+    public void savetodatabase(ActionEvent actionEvent) {
+    }
+}
